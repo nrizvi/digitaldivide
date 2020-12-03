c270463ff521171f7311ae90a19194a17330a1ed
zhangy111 <henryzusa@gmail.com>
2020-12-03 02:46:53 -0600

    created view to render data from json

--- digitaldivide.py
+++ digitaldivide.py
@@ -5,17 +5,40 @@ Created on Fri Nov 20 13:48:49 2020
 @author: NabaDev
 """
 
+import os
+from flask import Flask, Blueprint, render_template, request, make_response, json, jsonify, current_app
 
-from flask import Flask, Blueprint
+app = Blueprint('app', __name__)
 
-hello = Blueprint('hello', __name__)
+@app.route('/')
+def view():
+    requests_file = os.path.join(current_app.static_folder, 'requests.json')
+    with open (requests_file) as file:
+        data = json.load(file)
+    return render_template('view.html', data=data)
 
-@hello.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/donate')
+def donate():
+    return render_template('form.html')
+    #return 'Hello, World!'
+
+# below are sample code, not actually used
+@app.route('/postjson', methods = ['POST'])
+def postJsonHandler():
+    print (request.is_json)
+    content = request.get_json()
+    print (content)
+    return 'JSON posted'
+
+@app.route("/getjson", methods=["GET"])
+def starting_url():
+    json_data = request.json
+    a_value = json_data["a_key"]
+    return "JSON value sent: " + a_value
+# end sample code
 
 if __name__ == '__main__':
-    app = Flask(__name__)
-    app.register_blueprint(hello, url_prefix='/')
+    flask = Flask(__name__)
+    flask.register_blueprint(app, url_prefix='/')
 
-    app.run()
+    flask.run()
